b44ba5a71a234de0c4856ed365eb82a312372551
LordCodex <michaelolaitan6@gmail.com>
2023-12-06 20:43:20 +0100

    first commit

---
+++ src/views/HomeView.vue
@@ -0,0 +1,783 @@
+<script setup>
+
+</script>
+
+<template>
+  <div class="body-wrapper">
+    <header>
+      <div class="header w-90">
+        <div class="header-logo">
+          <!-- <img src="../assets/img/https://360lite.com.ng/assets/images/logo.png" alt=""> -->
+          <div class="social-circles" style="background-color: #feae37; ">
+            <i class="fab fa-twitter" style="color: white;"></i>
+          </div>
+          <h2>360litePay</h2>
+        </div>
+        <!-- <div class="header-nav-button"> -->
+        <div class="header-nav">
+          <ul>
+            <li>Home</li>
+            <li>About</li>
+            <li>Contact</li>
+            <li>Services</li>
+            <li>Faq</li>
+          </ul>
+        </div>
+        <div class="header-button">
+          <button>Get Started</button>
+        </div>
+        <div class="nav-icon">
+          <i class="fa-solid fa-bars"></i>
+        </div>
+        <!-- </div> -->
+      </div>
+    </header>
+    <aside>
+      <!-- for navbar mobile -->
+    </aside>
+    <main>
+      <section>
+        <div class="hero-section-container">
+          <div class="hero-section w-90">
+            <div class="hero-content flex-basis-50 w-50">
+              <h1>Welcome To 360litepay</h1>
+              <p>
+                360litepay provides an avenue for anyone in Nigeria to buy
+                airtime VTU, pay cable subscription, buy internet data, and
+                pay electricity bill and other daily services that you pay for
+                every day.
+              </p>
+              <div class="hero-buttons">
+                <button class="hero-btn-one">Get Started</button>
+                <button class="hero-btn-two">Sign in</button>
+              </div>
+            </div>
+            <div class="hero-img flex-basis-50 w-50">
+              <img
+                src="../assets/img/5.jpeg"
+                width="600px"
+                height="725px"
+                class="img-fluid rounded shadow" />
+            </div>
+          </div>
+        </div>
+      </section>
+      <section>
+        <div class="service-container">
+          <div class="service-section w-90">
+            <div class="service-contents">
+              <div class="service-box-contents">
+                <div class="box-content" style="background-color: #ecedfc">
+                  <div
+                    class="box-content-icon"
+                    style="background-color: #4759f939">
+                    <img src="../assets/img/6.svg" alt />
+                  </div>
+                  <h2>Worldwide Payment</h2>
+                  <p>
+                    Worldwide Payment require an operational approach and
+                    problem-solving methodology that is specifically tailored
+                    to drive improvements to the system.
+                  </p>
+                </div>
+                <div class="box-content-text">
+                  <h1 class="pc">
+                    Some distinguishing features that set 360litepay apart
+                    from others.
+                  </h1>
+                  <h1 class="mobile">Our Features</h1>
+                  <p>
+                    Exceptional Attributes that Differentiate 360litepay from
+                    Competitors
+                  </p>
+                  <button class="hero-btn-one">Get Started</button>
+                </div>
+                <div class="box-content" style="background-color: #fbedd9">
+                  <div
+                    class="box-content-icon"
+                    style="background-color: #feae3785">
+                    <img src="../assets/img/7.svg" alt />
+                  </div>
+                  <h2>Transaction fee is free!</h2>
+                  <p>
+                    Unlock cost-free transactions on our money sending app!
+                    Experience the ease of sending money without any fees.
+                    Enjoy seamless financial transactions.
+                  </p>
+                </div>
+                <div class="box-content" style="background-color: #effbf9">
+                  <div
+                    class="box-content-icon"
+                    style="background-color: #3cd09768">
+                    <img src="../assets/img/8.svg" alt />
+                  </div>
+                  <h2>Secure Transaction</h2>
+                  <p>
+                    Safeguard your sunny transactions with our Secure Transfer
+                    feature. Enjoy swift and secure money sending that keeps
+                    your financial rays shining bright.
+                  </p>
+                </div>
+                <!-- <div class="box-content"></div>
+                <div class="box-content"></div> -->
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+
+      <section style="margin: 0 auto">
+        <div class="why-us-container">
+          <div class="why-us-content w-90">
+            <div class="why-us-head">
+              <h1>Why Choose Us?</h1>
+              <p>
+                We make it easy for users to use our platform, that's why we
+                provide this benefit.
+              </p>
+            </div>
+            <div class="why-us-boxes">
+              <div class="why-us-box">
+                <div class="why-us-icon">
+                  <img src="../assets/img/cloud.svg" alt />
+                </div>
+                <h3>Cloud Drive</h3>
+                <p>
+                  A safe electronic funds transfer (EFT) is a fast and
+                  convenient way to send and receive money online.
+                </p>
+              </div>
+              <div class="why-us-box">
+                <div class="why-us-icon">
+                  <img src="../assets/img/bulb.svg" alt />
+                </div>
+                <h3>Payment Overview</h3>
+                <p>
+                  Payment Overview window, you can query a payment & review
+                  status and its related high-level information.
+                </p>
+              </div>
+              <div class="why-us-box">
+                <div class="why-us-icon">
+                  <img src="../assets/img/rocket.svg" alt />
+                </div>
+                <h3>Expertise Built</h3>
+                <p>
+                  Suitable or agreeable to the needs or purpose; well-suited
+                  with respect to facility or ease in use
+                </p>
+              </div>
+              <div class="why-us-box">
+                <div class="why-us-icon">
+                  <img src="../assets/img/search.svg" alt />
+                </div>
+                <h3>Convenient & Easy</h3>
+                <p>
+                  A safe electronic funds transfer (EFT) is a fast and
+                  convenient way to send and receive money online.
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+
+      <section>
+        <div class="download-app-container">
+          <div class="download-app-section w-90">
+            <div class="download-app-image">
+              <img src="../assets/img/phone.svg" alt style="
+              margin-bottom: -0.3em;
+          ">
+            </div>
+            <div class="download-app-text">
+              <h1>Getting Paid is now easier!</h1>
+              <p>
+                360litepay is fully reserved. Unlike banks, we don’t loan out
+                your money. We publish our holdings and obligations in real
+                time.
+              </p>
+              <div class="people">
+                <img src="../assets/img/users.svg" alt>
+                <p>More than 10,000 people have tried</p>
+              </div>
+              <button>Try Our App</button>
+            </div>
+          </div>
+        </div>
+      </section>
+    </main>
+    <footer>
+      <div class="footer-container">
+        <div class="footer-section">
+          <div class="footer-top-box">
+            <div class="footer-top-box-text flex-basis-50 w-50">
+              <h1>Download now! A great app for Sending money</h1>
+              <p>
+                Send your money to everywhere in the world in less than a minute! Download the app now.
+              </p>
+            </div>
+            <div class="footer-top-box-app-icons flex-basis-50 w-50">
+              <img src="../assets/img/App Store.svg" alt>
+              <img src="../assets/img/Goggle Play.svg" alt>
+            </div>
+          </div>
+          <div class="footer-bottom-logo-links">
+            <div class="footer-bottom-logo">
+              <div class="social-circles" style="background-color: white; ">
+                <i class="fab fa-twitter" style="color: #feae37;"></i>
+              </div>
+              <h2>360litePay</h2>
+            </div>
+            <div class="footer-bottom-links">
+              <ul>
+                <li>Features</li>
+                <li>Why Choose Us</li>
+                <li>Testimonial</li>
+                <li>FAQ</li>
+                <li>News</li>
+              </ul>
+            </div>
+          </div>
+          <hr style="width:100%;text-align:left;margin-left:0">
+          <div class="footer-copyright-links-socials">
+            <div class="footer-copyright">© 2023 PayPulse inc. All right reserved</div>
+            <div class="footer-links">
+              <ul>
+                <li>Privacy Policy</li>
+                <li>Terms & Condition</li>
+              </ul>
+            </div>
+            <div class="footer-socials ">
+              <div class="social-circles" style="background-color: #5061f9;">
+                <i class="fab fa-facebook"></i>
+              </div>
+              <div class="social-circles" style="background-color: #5061f9;">
+                <i class="fab fa-instagram"></i>
+              </div>
+              <div class="social-circles" style="background-color: white; ">
+                <i class="fab fa-twitter" style="color: #feae37;"></i>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </footer>
+  </div>
+</template>
+
+<style>
+@import url("https://fonts.googleapis.com/css2?family=Urbanist&display=swap");
+
+* {
+  padding: 0;
+  margin: 0;
+  text-decoration: none;
+  list-style-type: none;
+  box-sizing: border-box;
+  font-family: "Urbanist", sans-serif;
+}
+:root {
+  /* --primary: #03420d; */
+  --primary: #4759f9;
+  --secondary: #feae37;
+  --tertiary: #1d1e25;
+  --quaternary: #454c5d;
+  --light: #bdc3c7;
+  --dark: #2c3e50;
+  --white: #ffffff;
+  --black: #000000;
+  --font-family: "Roboto", sans-serif;
+  --transition: all 0.3s ease-in-out;
+  --primary-text: #2c3e50;
+  --secondary-text: #bdc3c7;
+  --link-color: var(--primary);
+  --hover-color: rgba(60, 63, 64, 0.05);
+  --active-color: var(--primary);
+  --border-radius: 3px;
+  --shadow: 0 2px 5px 0 rgba(0, 0, 0, 0.16);
+}
+
+body {
+  font-weight: 400;
+  font-style: normal;
+  scroll-timeline: inherit;
+  text-rendering: optimizeSpeed;
+  -moz-font-smoothing: antialiased;
+  scroll-behavior: smooth;
+  -webkit-tap-highlight-color: transparent;
+}
+.w-90 {
+  max-width: 80%;
+  margin: 0 auto;
+}
+.w-50 {
+  width: 50%;
+}
+.w-100 {
+  width: 100%;
+}
+.flex-basis-50 {
+  flex-basis: 50%;
+}
+.rounded {
+  border-radius: 6px !important;
+}
+.shadow {
+  -webkit-box-shadow: 0 5px 13px rgba(60, 72, 88, 0.2) !important;
+  box-shadow: 0 5px 13px rgba(60, 72, 88, 0.2) !important;
+}
+.img-fluid {
+  max-width: 100%;
+  height: auto;
+}
+button {
+  cursor: pointer;
+  font-weight: 700;
+  border: none;
+  padding: 14.5px 30px;
+  text-transform: uppercase;
+  font-size: 14px;
+  line-height: initial;
+  border-radius: 4px;
+}
+header {
+  padding: 1em 0;
+  border-bottom: 1px solid #e2e2e2;
+  box-shadow: rgba(33, 35, 38, 0.1) 0px 10px 10px -10px;
+}
+main {
+  padding: 1em 0;
+}
+section {
+  margin-bottom: 4em;
+}
+footer {
+  margin-top: 4em;
+}
+.header {
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  text-align: center;
+}
+
+.header-nav-button {
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  gap: 2em;
+  text-align: center;
+}
+.header-nav > ul {
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  gap: 2em;
+  text-align: center;
+}
+
+.header-button > button {
+  color: var(--white);
+  border: 1px solid var(--primary);
+  background: var(--primary);
+}
+.nav-icon {
+  display: none;
+  font-size: 30px;
+}
+
+.hero-section {
+  display: flex;
+  align-items: center;
+  /* margin-top: 1em; */
+  padding: 0.5em auto;
+}
+.hero-content {
+  padding-right: 2em;
+  display: flex;
+  flex-direction: column;
+  gap: 2em;
+}
+.hero-buttons {
+  display: flex;
+  gap: 1em;
+  align-items: center;
+}
+
+.hero-btn-one {
+  color: var(--white);
+  border: 1px solid var(--primary);
+  background: var(--primary);
+  padding: 14.5px 15px;
+}
+.hero-btn-two {
+  background: rgb(237, 235, 235);
+  padding: 14.5px 35px;
+}
+.hero-img {
+  height: 500px;
+  width: 100% !important;
+}
+.hero-img img {
+  width: 100% !important;
+  object-fit: cover;
+  height: 100%;
+}
+.service-box-contents {
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
+  column-gap: 25px;
+  row-gap: 15px;
+  align-items: center;
+  justify-content: center;
+}
+.box-content {
+  display: flex;
+  flex-direction: column;
+  gap: 0.8em;
+  padding: 3em;
+  border-radius: 15px;
+  -webkit-border-radius: 15px;
+  -moz-border-radius: 15px;
+  -ms-border-radius: 15px;
+  -o-border-radius: 15px;
+}
+.box-content > p {
+  font-size: 16px;
+}
+.box-content-icon {
+  height: 60px;
+  width: 60px;
+  border-radius: 50%;
+  padding: 1.5em;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+.box-content-text {
+  display: flex;
+  flex-direction: column;
+  padding: 1em 1em 1em 1em;
+  gap: 1.5em;
+  line-height: 1;
+}
+.box-content-text > button {
+  width: 50%;
+}
+.box-content-text > h1 {
+  font-size: 35px;
+  font-weight: 900;
+}
+.box-content-text > p {
+  font-size: 16px;
+}
+.mobile {
+  display: none;
+}
+.why-us-container {
+  background-color: #f4f5ff;
+  padding: 2em 0 4em 0;
+}
+.why-us-boxes {
+  display: grid;
+  grid-template-columns: repeat(4, 25%);
+  column-gap: 40px;
+  row-gap: 15px;
+  align-items: center;
+  justify-content: center;
+}
+
+.why-us-box {
+  padding: 4em;
+  background-color: white;
+  border-radius: 10px;
+  -webkit-border-radius: 10px;
+  -moz-border-radius: 10px;
+  -ms-border-radius: 10px;
+  -o-border-radius: 10px;
+}
+
+.why-us-head {
+  text-align: center;
+  margin: 2em auto;
+  line-height: 1;
+}
+.why-us-head h1 {
+  font-size: 25px;
+  font-weight: 900;
+}
+.why-us-head p {
+  font-size: 16px;
+  padding-top: 1em;
+}
+.why-us-box {
+  display: flex;
+  flex-direction: column;
+  padding: 1.8em;
+  gap: 0.8em;
+  color: var(--primary);
+}
+.why-us-box > p {
+  font-size: 16px;
+}
+.why-us-icon {
+  height: 50px;
+  width: 50px;
+  border-radius: 20%;
+  padding: 1.5em;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background-color: #f4f5ff;
+  -webkit-border-radius: 20%;
+  -moz-border-radius: 20%;
+  -ms-border-radius: 20%;
+  -o-border-radius: 20%;
+}
+
+.download-app-container {
+  background: var(--primary);
+  padding: 3em;
+  padding-bottom: 0;
+}
+.download-app-section {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  gap: 2em;
+}
+.download-app-text {
+  color: white;
+  display: flex;
+  flex-direction: column;
+  padding: 2em;
+  gap: 1.2em;
+}
+.people {
+  display: flex;
+  align-items: center;
+  font-size: 14px;
+  gap: 0.5em;
+}
+.download-app-text h1 {
+  font-size: 45px;
+  font-weight: 900;
+  line-height: 1.3;
+}
+.download-app-text > button {
+  max-width: 50%;
+}
+.download-app-image img {
+  width: 100% !important;
+  object-fit: cover;
+  height: 100%;
+}
+
+.footer-container {
+  background-color: var(--primary);
+  padding: 100px 100px 10px 100px;
+
+  color: white;
+}
+.footer-top-box {
+  background-color: var(--secondary);
+  display: flex;
+  padding: 5em;
+  margin-top: -10em;
+  border-radius: 20px;
+  align-items: center;
+  justify-content: center;
+  -webkit-border-radius: 20px;
+  -moz-border-radius: 20px;
+  -ms-border-radius: 20px;
+  -o-border-radius: 20px;
+}
+.footer-top-box-text {
+  display: flex;
+  flex-direction: column;
+  gap: 1em;
+}
+.footer-bottom-logo-links{
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+  margin-top: 2em;
+  gap: 2em;
+}
+.footer-bottom-links> ul{
+  display: flex;
+  gap: 1em;
+  align-items: center;
+  flex-wrap: wrap;
+  text-align: center;
+  justify-content: center;
+}
+.header-logo,.footer-bottom-logo {
+  display: flex;
+  align-items: center;
+  gap: 0.5em;
+}
+hr{
+  margin-top:2em;
+}
+.footer-copyright-links-socials {
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  margin-top: 1em;
+}
+.footer-links > ul {
+  display: flex;
+  gap: 1em;
+  align-items: center;
+}
+.footer-socials {
+  display: flex;
+  gap: 1em;
+  align-items: center;
+}
+.footer-top-box-app-icons {
+  gap: 1em;
+  margin-left: 8em;
+}
+.social-circles {
+  height: 40px;
+  width: 40px;
+  background-color: red;
+  border-radius: 50%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  padding: 1em;
+}
+
+
+@media (max-width: 900px) {
+  section {
+    margin-bottom: 2em;
+  }
+  .header-nav,
+  .header-button {
+    display: none;
+  }
+  .nav-icon {
+    display: block;
+  }
+  .hero-section {
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    gap: 2em;
+    margin-top: 20px;
+  }
+  .flex-basis-50 {
+    flex-basis: 100% !important;
+  }
+  .w-50 {
+    width: 100% !important;
+  }
+  .hero-content {
+    text-align: center;
+    justify-content: center;
+    align-items: center;
+    padding: 0;
+  }
+  .why-us-boxes,
+  .service-box-contents {
+    grid-template-columns: repeat(1, 1fr);
+    row-gap: 20px;
+  }
+  .service-box-contents > .box-content-text {
+    order: -1;
+  }
+  .box-content {
+    padding: 2em;
+  }
+
+  .box-content-text {
+    padding: 1em 0em;
+    text-align: center;
+  }
+  .box-content-text > h1 {
+    font-size: 25px;
+    font-weight: 900;
+  }
+  .box-content-text > button {
+    display: none;
+  }
+  .mobile {
+    display: block;
+  }
+  .pc {
+    display: none;
+  }
+  .download-app-section {
+    flex-direction: column-reverse;
+    gap: 3em;
+  }
+  .download-app-container {
+    padding: 3em 0 0 0;
+  }
+  .download-app-text {
+    padding: 0;
+  }
+
+  .download-app-text h1 {
+    font-size: 40px;
+    font-weight: 900;
+    line-height: 1.3;
+  }
+  .download-app-text > button {
+    width: 50%;
+  }
+  .footer-container {
+    background-color: var(--primary);
+    padding: 100px;
+  }
+  .footer-top-box {
+    flex-direction: column;
+    margin-top: 10em;
+    gap: 2em;
+  }
+  .footer-top-box-text {
+    display: flex;
+    flex-direction: column;
+    gap: 1em;
+  }
+  .footer-top-box-app-icons {
+    display: flex;
+    align-items: center;
+    gap: 1em;
+  }
+  .footer-top-box-app-icons img {
+    width: 50%;
+  }
+  .footer-container {
+    background-color: var(--primary);
+    padding: 30px;
+  }
+  .footer-top-box {
+    padding: 2em;
+    margin-top: -7em;
+  }
+  .footer-top-box-text h1 {
+    font-size: 30px;
+
+  }
+  .footer-top-box-app-icons {
+    flex-direction: column;
+    justify-content: center;
+    gap: 1em;
+    margin-left: 0em;
+  }
+  .footer-copyright-links-socials{
+    flex-direction: column;
+    gap: 1.5em;
+  }
+}
+
+
+
+</style>
